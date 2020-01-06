cb7e825a849630ee38ba8ccf61716eca49a706ba
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2020-01-06 20:09:45 +0530

    added local storage for temporay

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 
 import Welcome from "./welcome/welcome";
-import UserHome from './userHome/UserHome';
+import User from "./user/User";
 
 class MainScreen extends Component {
-  state = { isUserLogined: true };
+  state = { isUserLogined: false };
 
-  handleUserLogin = () => {
+  handleUserLogin = ({ name, id }) => {
     this.setState({ isUserLogined: true });
+    localStorage.setItem("name", name);
+    localStorage.setItem("id", id);
+  };
+
+  componentDidMount = () => {
+    const name = localStorage.getItem("name");
+    const id = localStorage.getItem("id");
+
+    if (name && id) this.setState({ isUserLogined: true });
   };
   render() {
-    if (this.state.isUserLogined) return <UserHome />;
+    if (this.state.isUserLogined) return <User />;
     else return <Welcome handleLogin={this.handleUserLogin} />;
   }
 }
