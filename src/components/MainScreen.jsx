4bc7f1f5c41d701ab4715f1a762c80010ea33f69
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2020-01-09 23:16:22 +0530

    changes regarding user login

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -4,22 +4,30 @@ import Welcome from "./welcome/welcome";
 import User from "./user/User";
 
 class MainScreen extends Component {
-  state = { isUserLogined: false };
-
-  handleUserLogin = ({ name, id }) => {
-    this.setState({ isUserLogined: true });
-    localStorage.setItem("name", name);
-    localStorage.setItem("id", id);
-  };
+  state = { isUserLogined: false, userInfo: {} };
 
   componentDidMount = () => {
     const name = localStorage.getItem("name");
     const id = localStorage.getItem("id");
-
     if (name && id) this.setState({ isUserLogined: true });
   };
+
+  // handlers
+
+  handleUserLogin = data => {
+    this.setState({ isUserLogined: true, userInfo: data });
+    localStorage.setItem("name", data.name);
+    localStorage.setItem("id", data.id);
+  };
+
+  handleLogout = () => {
+    this.setState({ isUserLogined: false, userInfo: {} });
+  };
   render() {
-    if (this.state.isUserLogined) return <User />;
+    if (this.state.isUserLogined)
+      return (
+        <User userInfo={this.state.userInfo} handleLogout={this.handleLogout} />
+      );
     else return <Welcome handleLogin={this.handleUserLogin} />;
   }
 }
