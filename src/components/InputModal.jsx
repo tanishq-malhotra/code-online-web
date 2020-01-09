c273c2f652b741ddcda38d872d9efe6c8393fa23
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2020-01-09 23:14:41 +0530

    project language category added

--- src/components/InputModal.jsx
+++ src/components/InputModal.jsx
@@ -1,26 +1,51 @@
 import React, { Component } from "react";
-import { Button, Header, Icon, Modal, Input } from "semantic-ui-react";
+import {
+  Button,
+  Header,
+  Icon,
+  Modal,
+  Input,
+  Dropdown,
+  Form
+} from "semantic-ui-react";
 
+const languageOptions = [
+  { key: "c", text: "c", value: "c" },
+  { key: "c++", text: "c++", value: "c++" },
+  { key: "python", text: "python", value: "python" }
+];
 class InputModal extends Component {
-  state = { input: "" };
+  state = { input: "", dropdown:'Select Language' };
   handleChange = (e, { name, value }) => this.setState({ [name]: value });
   render() {
     return (
       <div>
-        <Modal
-          basic
-          size="small"
-          dimmer="blurring"
-          open={this.props.visible}
-        >
+        <Modal basic size="small" dimmer="blurring" open={this.props.visible}>
           <Header icon="code" content="New Project" />
           <Modal.Content>
-            <Input
-              placeholder={this.props.inputPlaceHolder}
-              fluid
-              onChange={this.handleChange}
-              name="input"
-            />
+            <Form>
+              <Input
+                placeholder={this.props.inputPlaceHolder}
+                fluid
+                onChange={this.handleChange}
+                name="input"
+              />
+              <Form.Group></Form.Group>
+              <Form.Group></Form.Group>
+              <Dropdown
+                button
+                className="icon"
+                floating
+                labeled
+                icon="world"
+                options={languageOptions}
+                search
+                text={this.state.dropdown}
+                value = 'c'
+                name='dropdown'
+                onChange={this.handleChange}
+              />
+            </Form>
           </Modal.Content>
           <Modal.Actions>
             <Button
@@ -31,7 +56,11 @@ class InputModal extends Component {
             >
               <Icon name="remove" /> Close
             </Button>
-            <Button color="green" inverted>
+            <Button
+              color="green"
+              inverted
+              onClick={() => this.props.handleCreate(this.state.input, this.state.dropdown)}
+            >
               <Icon name="checkmark" /> {this.props.ButtonName}
             </Button>
           </Modal.Actions>
