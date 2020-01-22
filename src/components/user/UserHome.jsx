c62a5765a9e8757d824350b89c144254ac2889e0
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2020-01-22 12:21:13 +0530

    user settings and home page

---
+++ src/components/user/UserHome.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class UserHome extends Component {
+    state = {  }
+    render() { 
+        return ( <h1>Home</h1> );
+    }
+}
+ 
+export default UserHome;
